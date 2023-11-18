31237c539524641b9b5e7dba5f090a0187328428
tasinchowdhury26 <tasinchowdhury26@gmail.com>
2023-11-18 21:22:13 +0600

    first commit

---
+++ src/pages/index.js
@@ -0,0 +1,12 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Tasins Portfolio</title>
+      </Head>
+      <h1>Tasin POrtfolio</h1>
+    </div>
+  );
+}
